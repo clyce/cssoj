56d2a516e48bd59c93a4ef36220fbead6bf9d1aa
clyce <epi.clyce@gmail.com>
2013-04-15 09:59:24 +0800

    animation and column

--- src/cssoj/core.clj
+++ src/cssoj/core.clj
@@ -1,12 +1,16 @@
 (ns cssoj.core)
 
-(defn sep-to-str [sep coll]
+(defn sep-to-str 
+  ;TODO: write the doc 
+  [sep coll]
   (apply str
          (interleave coll 
                      (repeat (dec (count coll)) 
                              sep))))
 
-(defn value-to-str [coll]
+(defn value-to-str 
+  ;TODO: write the doc 
+  [coll]
   (if (coll? coll)
     ((if (map? coll) 
        mapcat map) value-to-str coll)  
@@ -16,7 +20,9 @@
         coll
         (str coll)))))
 
-(defn- process-attr [attr]
+(defn- process-attr 
+  ;TODO: write the doc 
+  [attr]
   (if (string? attr) 
     attr
     (apply str 
@@ -24,7 +30,9 @@
         (reduce #(str % ", " %2) (map process-attr attr)) 
         (reduce #(str % " " %2) attr)))))
 
-(defn- process-pair [pair]
+(defn- process-pair 
+  ;TODO: write the doc 
+  [pair]
   (let [pair (value-to-str pair)]
     (if (coll? pair)
       (apply str
@@ -32,13 +40,17 @@
           (partition 2 (if (map? pair) (apply concat pair) pair)))) 
       pair)))
 
-(defn process-nest [p c]
+(defn process-nest 
+  ;TODO: write the doc 
+  [p c]
   (let [c (value-to-str c)]
     (condp = (first c)
       \& (str p c)
       (str p " " c))))
 
-(defn prettify [css-string]
+(defn prettify 
+  ;TODO: write the doc 
+  [css-string]
   (apply str 
     (map #(condp = %
             \{ "{\n  "
@@ -47,12 +59,16 @@
             %)
       css-string)))
 
-(defn style [& style-list]
+(defn style 
+  ;TODO: write the doc 
+  [& style-list]
   (apply str
     (map process-pair
       style-list)))
 
-(defn onto [[ele stl & children]] 
+(defn onto 
+  ;TODO: write the doc 
+  [[ele stl & children]] 
   (let [ele (value-to-str ele)]
     (apply str
       (str ele " {" 
@@ -63,8 +79,15 @@
                  (rest %)))
         children))))
 
-(defn apply-to [& args]
+(defn apply-to 
+  ;TODO: write the doc 
+  [& args]
   (apply str
     (map 
       #(if (string? %) % (onto %))
       args)))
+
+(defn add-unit 
+  ;TODO: write the doc 
+  [unit v]
+  (str (value-to-str v) (if (number? v) (value-to-str unit))))
--- src/cssoj/css3.clj
+++ src/cssoj/css3.clj
@@ -2,7 +2,9 @@
   (:use [cssoj.core]))
 
 (defn border 
+  ;TODO: write the doc 
   [& {:keys [attrs image radius shadow]}]
+  ;TODO: test me  
   (style
     (if attrs
       (style [:border attrs]))
@@ -17,17 +19,14 @@
       (style [:box-shadow shadow]))))
 
 (defn background
+  ;TODO: write the doc 
   [& {:keys [attrs clip origin size]}]
-  (style
-    (if attrs
-      (style [:background attrs]))
-    (if clip
-      (style [:background-clip clip]))
-    (if origin
-      (style [:background-origin origin]))
-    (if size
-      (style [:background-size size
-              :-moz-background-size size]))))
+  ;TODO: test me  
+  (style (if attrs (style [:background attrs]))
+         (if clip (style [:background-clip clip]))
+         (if origin (style [:background-origin origin]))
+         (if size (style [:background-size size
+                          :-moz-background-size size]))))
 
 (defn text
   [& {:keys []}]
@@ -39,12 +38,17 @@
   ;TODO: complete this
   )
 
-(defn gen-css-matrix [coll]
-  (if (= 6 (count coll))
-    (str "matrix(" (apply str (interleave coll (repeat (dec (count coll)) ","))) ")")
-    (str "matrix3d(" (apply str (interleave coll (repeat (dec (count coll)) ","))) ")")))
+(defn gen-css-matrix 
+  ;TODO: write the doc 
+  [coll]
+  ;TODO: test me
+  (str (if (= 6 (count coll))
+         "matrix(" "matrix3d(") 
+       (sep-to-str "," coll) 
+       ")"))
 
 (defn transform
+  ;TODO: write the doc 
   [& {:keys [translate
              rotate 
              scale 
@@ -57,87 +61,71 @@
              matrix]
       :or {backface true
            child3d true}}]
-  (style 
-    (if backface
-       [:backface-visibility :visible
-        :-webkit-backface-visibility :visible 
-        :-moz-backface-visibility :visible 
-        :-ms-backface-visibility :visible]
-       [:backface-visibility :hidden
-        :-webkit-backface-visibility :hidden 
-        :-moz-backface-visibility :hidden 
-        :-ms-backface-visibility :hidden]) 
-
-     (if persp-origin
-       [:perspective-origin persp-origin
-        :-webkit-perspective-origin persp-origin]) 
-
-     (if persp
-       [:perspective persp
-        :-webkit-perspective persp]) 
-
-     (if child3d
-       [:transform-style :preserve-3d
-        :-webkit-transform-style :preserve-3d]
-       [:transform-style :flat
-        :-webkit-transform-style :flat])
-
-     (if origin
-       [:transform-origin origin 
-        :-ms-transform-origin origin  
-        :-webkit-transform-origin origin  
-        :-moz-transform-origin origin 
-        :-o-transform-origin origin])
-
-     (if (or matrix translate rotate scale skew)
-       (let 
-         [t (if matrix
-              (gen-css-matrix matrix)
-              [(if translate 
-                 (let [[x y z] translate] 
-                   (if z
-                     (str "translate3d(" x "," y "," z ")")
-                     (str "translate(" x "," y ")"))))
-               (if rotate
-                 (if (coll? rotate)
-                   (let [[x y z] (map #(if (number? %) (str % "deg") %) rotate)]
-                     (str "rotateX(" x ") "
-                          "rotateY(" y ") "
-                          "rotateZ(" z ")"))
-                   (str "rotate(" (if (number? rotate) (str rotate "deg") rotate) ")")))
-               (if scale
-                 (let [[x y z] scale] 
-                   (if z
-                     (str "scale3d(" x "," y "," z ")")
-                     (str "scale(" x "," y ")"))))
-               (if skew
-                 (let [[x y] skew]
-                   (str "skew(" x "," y ")")))])]
-         [:transform t
-          :-ms-transform t 
-          :-moz-transform t 
-          :-webkit-transform t 
-          :-o-transform t]))))
+  (style (if backface
+           [:backface-visibility :visible
+            :-webkit-backface-visibility :visible 
+            :-moz-backface-visibility :visible 
+            :-ms-backface-visibility :visible]
+           [:backface-visibility :hidden
+            :-webkit-backface-visibility :hidden 
+            :-moz-backface-visibility :hidden 
+            :-ms-backface-visibility :hidden]) 
+         (if persp-origin [:perspective-origin persp-origin
+                           :-webkit-perspective-origin persp-origin]) 
+         (if persp [:perspective persp
+                    :-webkit-perspective persp]) 
+         (if child3d
+           [:transform-style :preserve-3d
+            :-webkit-transform-style :preserve-3d]
+           [:transform-style :flat
+            :-webkit-transform-style :flat])
+         (if origin [:transform-origin origin 
+                     :-ms-transform-origin origin  
+                     :-webkit-transform-origin origin  
+                     :-moz-transform-origin origin 
+                     :-o-transform-origin origin])
+         (if (or matrix translate rotate scale skew)
+           (let [t (if matrix
+                     (gen-css-matrix matrix)
+                     [(if translate 
+                        (let [[x y z] translate] 
+                          (if z (str "translate3d(" x "," y "," z ")") (str "translate(" x "," y ")"))))
+                      (if rotate
+                        (if (coll? rotate)
+                          (let [[x y z] (map (partial add-unit "deg") rotate)]
+                            (str "rotateX(" x ") " "rotateY(" y ") " "rotateZ(" z ")"))
+                          (str "rotate(" (add-unit "deg" rotate) ")")))
+                      (if scale
+                        (let [[x y z] scale] 
+                          (if z (str "scale3d(" x "," y "," z ")") (str "scale(" x "," y ")"))))
+                      (if skew
+                        (let [[x y] skew] (str "skew(" x "," y ")")))])]
+             [:transform t
+              :-ms-transform t 
+              :-moz-transform t 
+              :-webkit-transform t 
+              :-o-transform t]))))
 
 (defn transition
+  ;TODO: write the doc 
   [& transitions] 
   ;TODO: test me
   (let 
-    [v ((partial sep-to-str ", ")
-          (for [t (value-to-str transitions)]
-            (let [[prop duration timing start] t]
-              (str prop " " 
-                   (str duration 
-                        (if (number? duration) "s")) " "
-                   timing " "
-                   (str start
-                        (if (number? start) "s"))))))]
+    [v [(for [t (value-to-str transitions)]
+          (let [[prop duration timing start] t]
+            [prop 
+             (str (value-to-str duration) 
+                  (if (number? duration) "s")) 
+             timing 
+             (str (value-to-str start) 
+                  (if (number? start) "s"))]))]]
     (style [:transition v
             :-moz-transition v
             :-o-transition v
             :-webkit-transition v])))
 
 (defn apply-transition
+  ;TODO: write the doc 
   [ele transitions & {:keys [on to]}]
   ;TODO: test me
   (let [[ele transitions] (value-to-str [ele transitions])] 
@@ -147,17 +135,63 @@
       (onto [ele [(transition transitions)]]))))
 
 (defn keyframe
+  ;TODO: write the doc 
   [kfname & key-points]
   ;TODO: test me
-  (let [flow 
-        (str " " kfname " {"
-             (apply apply-to 
-                    (map #(list (str (value-to-str (first %)) 
-                                     (if (number? (first %)) "%"))
-                                (second %)) key-points))
-             "}\n"
-             )]
+  (let [flow (str " " (value-to-str kfname) " {"
+                  (apply apply-to 
+                         (map #(list (add-unit "%" (first %))
+                                     (second %)) key-points))
+                  "}\n")]
     (str "@keyframes" flow
          "@-moz-keyframes" flow
          "@-webkit-keyframes" flow
          "@-o-keyframes" flow)))
+
+(defn animation
+  ;TODO: write the doc 
+  [aname duration & {:keys [timing start-delay iteration direction state]}]
+  ;TODO: test me
+  (let [v [aname (add-unit "s" duration)
+           timing (add-unit start-delay)
+           iteration direction state]]
+    (style [:animation v
+            :-moz-animation v
+            :-webkit-animation v
+            :-o-animation v])))
+
+(defn column
+  ;TODO: write the doc 
+  [colcount & {:keys [width fill gap rule-color rule-style rule-width colspan]}]
+  ;TODO: test me
+  (style [:colunm-count colcount
+          :-moz-colunm-count colcount
+          :-webkit-colunm-count colcount]
+         (if width 
+           (let [width (add-unit "px" width)] 
+             [:column-width width
+              :-moz-column-width width
+              :-webkit-column-width width]))
+         (if fill [:column-fill fill
+                   :-moz-column-fill fill
+                   :-webkit-column-fill fill])
+         (if gap 
+           (let [gap (add-unit "px" gap)] 
+             [:column-gap gap
+              :-moz-column-gap gap
+              :-webkit-column-gap gap]))
+         (if rule-color [:column-rule-color rule-color
+                         :-moz-column-rule-color rule-color
+                         :-webkit-column-rule-color rule-color])
+         (if rule-style [:column-rule-style rule-style
+                         :-moz-column-rule-style rule-style
+                         :-webkit-column-rule-style rule-style])
+         (if rule-width 
+           (let [rule-width (add-unit "px" rule-width)] 
+             [:column-rule-width rule-width
+              :-moz-column-rule-width rule-width
+              :-webkit-column-rule-width rule-width]))
+         (if colspan
+           [:column-span colspan
+            :-moz-column-span colspan
+            :-webkit-column-span colspan])))
